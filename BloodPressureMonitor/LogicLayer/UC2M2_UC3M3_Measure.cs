4f6a2b4c7f6d08920e498162bbe5aacc6216c5a4
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2018-10-31 10:06:06 +0100

    Projekterne er genoprettet som Framework i stedet for Standard

---
+++ BloodPressureMonitor/LogicLayer/UC2M2_UC3M3_Measure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    class UC2M2_UC3M3_Measure : IMeasure
+    {
+    }
+}
--- BloodPressureMonitor/Main/Properties/AssemblyInfo.cs
+++ BloodPressureMonitor/Main/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("45ce59cf-2530-449c-abe6-2a8c3dc5da5c")]
+[assembly: Guid("800e7f58-5343-4eb4-bd57-ecd4c9d0feaf")]
 
 // Version information for an assembly consists of the following four values:
 //
