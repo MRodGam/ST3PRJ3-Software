c82d5cab06652a787885d336cef3eb24bb71ec2a
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2018-11-07 08:49:08 +0100

    Referencer tilføjet

--- BloodPressureMonitor/LogicLayer/UC4M4_SaveData.cs
+++ BloodPressureMonitor/LogicLayer/UC4M4_SaveData.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DTO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System.Data.SqlClient;
+using DataLayer;
+using Domain;
 
 namespace LogicLayer
 {
