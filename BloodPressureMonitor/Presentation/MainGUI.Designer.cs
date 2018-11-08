4741e2eba654fd5dd6cdd7cb8cb392005b2fde5e
Laura Noergaard <au559056@post.au.dk>
2018-11-08 11:54:04 +0100

    main gui påbegyndt

--- BloodPressureMonitor/Presentation/MainGUI.Designer.cs
+++ BloodPressureMonitor/Presentation/MainGUI.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.StartB = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // StartB
@@ -36,19 +39,51 @@
             this.StartB.BackColor = System.Drawing.Color.ForestGreen;
             this.StartB.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StartB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.StartB.Location = new System.Drawing.Point(13, 381);
+            this.StartB.Location = new System.Drawing.Point(10, 310);
+            this.StartB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.StartB.Name = "StartB";
-            this.StartB.Size = new System.Drawing.Size(198, 57);
+            this.StartB.Size = new System.Drawing.Size(148, 46);
             this.StartB.TabIndex = 0;
             this.StartB.Text = "START MÅLING";
             this.StartB.UseVisualStyleBackColor = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(479, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Grænseværdi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Gem";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(479, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Ryd";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // MainGUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.StartB);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainGUI";
             this.Text = "MainGUI";
             this.ResumeLayout(false);
@@ -58,5 +93,8 @@
         #endregion
 
         private System.Windows.Forms.Button StartB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
