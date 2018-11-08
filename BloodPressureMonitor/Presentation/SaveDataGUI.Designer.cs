87c5cfe07d4fe3ada4e267110f1f8e1687b62313
Laura Noergaard <au559056@post.au.dk>
2018-11-08 12:13:22 +0100

    Gem Data GUI

--- BloodPressureMonitor/Presentation/SaveDataGUI.Designer.cs
+++ BloodPressureMonitor/Presentation/SaveDataGUI.Designer.cs
@@ -46,22 +46,24 @@
             // 
             // SaveB
             // 
+            this.SaveB.BackColor = System.Drawing.Color.Silver;
             this.SaveB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveB.ForeColor = System.Drawing.Color.Black;
-            this.SaveB.Location = new System.Drawing.Point(290, 413);
+            this.SaveB.ForeColor = System.Drawing.Color.White;
+            this.SaveB.Location = new System.Drawing.Point(218, 336);
             this.SaveB.Margin = new System.Windows.Forms.Padding(0);
             this.SaveB.Name = "SaveB";
-            this.SaveB.Size = new System.Drawing.Size(156, 37);
+            this.SaveB.Size = new System.Drawing.Size(117, 30);
             this.SaveB.TabIndex = 64;
             this.SaveB.Text = "Gem";
-            this.SaveB.UseVisualStyleBackColor = true;
+            this.SaveB.UseVisualStyleBackColor = false;
             // 
             // medarbejderIDTB
             // 
             this.medarbejderIDTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.medarbejderIDTB.Location = new System.Drawing.Point(182, 109);
+            this.medarbejderIDTB.Location = new System.Drawing.Point(136, 89);
+            this.medarbejderIDTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.medarbejderIDTB.Name = "medarbejderIDTB";
-            this.medarbejderIDTB.Size = new System.Drawing.Size(222, 34);
+            this.medarbejderIDTB.Size = new System.Drawing.Size(168, 28);
             this.medarbejderIDTB.TabIndex = 63;
             this.medarbejderIDTB.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
             // 
@@ -69,10 +71,10 @@
             // 
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.Color.LightGray;
-            this.label13.Location = new System.Drawing.Point(19, 113);
+            this.label13.Location = new System.Drawing.Point(14, 92);
             this.label13.Margin = new System.Windows.Forms.Padding(0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(161, 43);
+            this.label13.Size = new System.Drawing.Size(121, 35);
             this.label13.TabIndex = 62;
             this.label13.Text = "MedarbejderID";
             // 
@@ -80,38 +82,40 @@
             // 
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Control;
-            this.label6.Location = new System.Drawing.Point(297, 215);
+            this.label6.Location = new System.Drawing.Point(223, 175);
             this.label6.Margin = new System.Windows.Forms.Padding(0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(24, 36);
+            this.label6.Size = new System.Drawing.Size(18, 29);
             this.label6.TabIndex = 61;
             this.label6.Text = "-";
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(314, 215);
+            this.textBox4.Location = new System.Drawing.Point(236, 175);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.MaxLength = 4;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(90, 34);
+            this.textBox4.Size = new System.Drawing.Size(68, 28);
             this.textBox4.TabIndex = 60;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(182, 215);
+            this.textBox2.Location = new System.Drawing.Point(136, 175);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(112, 34);
+            this.textBox2.Size = new System.Drawing.Size(85, 28);
             this.textBox2.TabIndex = 59;
             // 
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.LightGray;
-            this.label5.Location = new System.Drawing.Point(19, 220);
+            this.label5.Location = new System.Drawing.Point(14, 179);
             this.label5.Margin = new System.Windows.Forms.Padding(0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(142, 36);
+            this.label5.Size = new System.Drawing.Size(106, 29);
             this.label5.TabIndex = 58;
             this.label5.Text = "Patients CPR";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -120,10 +124,10 @@
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(19, 21);
+            this.label4.Location = new System.Drawing.Point(14, 17);
             this.label4.Margin = new System.Windows.Forms.Padding(0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(457, 59);
+            this.label4.Size = new System.Drawing.Size(343, 48);
             this.label4.TabIndex = 57;
             this.label4.Text = "Indtast venligst følgende oplysninger for at gemme i database ";
             // 
@@ -131,19 +135,20 @@
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(182, 351);
+            this.dateTimePicker1.Location = new System.Drawing.Point(136, 285);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(222, 30);
+            this.dateTimePicker1.Size = new System.Drawing.Size(168, 26);
             this.dateTimePicker1.TabIndex = 56;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.LightGray;
-            this.label1.Location = new System.Drawing.Point(19, 351);
+            this.label1.Location = new System.Drawing.Point(14, 285);
             this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 43);
+            this.label1.Size = new System.Drawing.Size(68, 35);
             this.label1.TabIndex = 55;
             this.label1.Text = "Dato";
             // 
@@ -151,47 +156,49 @@
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.LightGray;
-            this.label2.Location = new System.Drawing.Point(19, 268);
+            this.label2.Location = new System.Drawing.Point(14, 218);
             this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 36);
+            this.label2.Size = new System.Drawing.Size(106, 29);
             this.label2.TabIndex = 65;
             this.label2.Text = "Patients navn";
             // 
             // navnTB
             // 
             this.navnTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.navnTB.Location = new System.Drawing.Point(182, 268);
+            this.navnTB.Location = new System.Drawing.Point(136, 218);
+            this.navnTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.navnTB.Multiline = true;
             this.navnTB.Name = "navnTB";
-            this.navnTB.Size = new System.Drawing.Size(222, 65);
+            this.navnTB.Size = new System.Drawing.Size(168, 54);
             this.navnTB.TabIndex = 66;
             // 
             // procedureTB
             // 
             this.procedureTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.procedureTB.Location = new System.Drawing.Point(182, 159);
+            this.procedureTB.Location = new System.Drawing.Point(136, 129);
+            this.procedureTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.procedureTB.Name = "procedureTB";
-            this.procedureTB.Size = new System.Drawing.Size(222, 34);
+            this.procedureTB.Size = new System.Drawing.Size(168, 28);
             this.procedureTB.TabIndex = 67;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.LightGray;
-            this.label3.Location = new System.Drawing.Point(18, 159);
+            this.label3.Location = new System.Drawing.Point(14, 129);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(161, 43);
+            this.label3.Size = new System.Drawing.Size(121, 35);
             this.label3.TabIndex = 68;
             this.label3.Text = "Procedure";
             // 
             // SaveDataGUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(479, 474);
+            this.ClientSize = new System.Drawing.Size(359, 385);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.procedureTB);
             this.Controls.Add(this.navnTB);
@@ -206,6 +213,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SaveDataGUI";
             this.Text = "SaveDataGUI";
             this.ResumeLayout(false);
