eec542a28be7d3c2626dbbfd684bb8af10b5f26a
Laura Noergaard <au559056@post.au.dk>
2018-11-08 12:17:46 +0100

    v

--- BloodPressureMonitor/Presentation/MainGUI.Designer.cs
+++ BloodPressureMonitor/Presentation/MainGUI.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pauseB = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // StartB
@@ -40,7 +41,7 @@
             this.StartB.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StartB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.StartB.Location = new System.Drawing.Point(10, 310);
-            this.StartB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.StartB.Margin = new System.Windows.Forms.Padding(2);
             this.StartB.Name = "StartB";
             this.StartB.Size = new System.Drawing.Size(148, 46);
             this.StartB.TabIndex = 0;
@@ -80,17 +81,27 @@
             this.button3.Text = "Ryd";
             this.button3.UseVisualStyleBackColor = false;
             // 
+            // pauseB
+            // 
+            this.pauseB.Location = new System.Drawing.Point(222, 312);
+            this.pauseB.Name = "pauseB";
+            this.pauseB.Size = new System.Drawing.Size(75, 23);
+            this.pauseB.TabIndex = 4;
+            this.pauseB.Text = "Kvitter alarm ";
+            this.pauseB.UseVisualStyleBackColor = true;
+            // 
             // MainGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InfoText;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.pauseB);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.StartB);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainGUI";
             this.Text = "MainGUI";
             this.ResumeLayout(false);
@@ -103,5 +114,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button pauseB;
     }
 }
