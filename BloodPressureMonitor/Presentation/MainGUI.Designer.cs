81e463d635b72bbba849b2322772487e1ff3bdb6
Laura Noergaard <au559056@post.au.dk>
2018-11-08 11:59:54 +0100

    main

--- BloodPressureMonitor/Presentation/MainGUI.Designer.cs
+++ BloodPressureMonitor/Presentation/MainGUI.Designer.cs
@@ -49,35 +49,42 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(479, 230);
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(468, 248);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 31);
+            this.button1.Size = new System.Drawing.Size(120, 25);
             this.button1.TabIndex = 1;
             this.button1.Text = "Grænseværdi";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(479, 268);
+            this.button2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(468, 279);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(120, 25);
             this.button2.TabIndex = 2;
             this.button2.Text = "Gem";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(479, 298);
+            this.button3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(468, 310);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(120, 25);
             this.button3.TabIndex = 3;
             this.button3.Text = "Ryd";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // MainGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
             this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
