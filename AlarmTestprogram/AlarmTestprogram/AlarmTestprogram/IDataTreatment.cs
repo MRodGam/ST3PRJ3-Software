fc5282d786356b17b0a1a434a15c481fc80e7e16
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2018-12-05 14:31:36 +0100

    Oprettelse af alarmtestprogram

---
+++ AlarmTestprogram/AlarmTestprogram/AlarmTestprogram/IDataTreatment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace LogicLayer
+{
+    public interface IDataTreatment
+    {
+        void StartGraphData();
+        void StopGraphData();
+        void GetRawData();
+        void MakeShortRawList();
+        List<ConvertedData> GetGraphList();
+        List<RawData> GetFilterList();
+
+        //Filter
+        //void StartFilter();
+        //void StopFilter();
+        //void FilterData();
+        //List<RawData> GetFilterData(); 
+    }
+}
