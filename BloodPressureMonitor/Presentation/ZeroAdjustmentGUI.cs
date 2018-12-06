9cb523fda47612cd98eac7642f7c4f1b505cd303
Celia <au582514@post.au.dk>
2018-12-06 13:31:00 +0100

    kode til ZeroAdjustment. Vinduet for UCZero skal vises først, og kun hvis denne udføres skal hovedvinduet vises

--- BloodPressureMonitor/Presentation/ZeroAdjustmentGUI.cs
+++ BloodPressureMonitor/Presentation/ZeroAdjustmentGUI.cs
@@ -7,18 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LogicLayer;
+using Domain;
 
 namespace Presentation
 {
     public partial class ZeroAdjustmentGUI : Form
     {
-        public ZeroAdjustmentGUI()
+        private IZeroAdjustment _zeroAdjustment;
+        private UC1M1_ZeroAdjustment _uc1M1Zero;
+
+        public bool IsZeroAdjustmentMeasured { get; set; } = false;
+        
+        public ZeroAdjustmentGUI(IZeroAdjustment zeroAdjustment, UC1M1_ZeroAdjustment uc1M1Zero)
         {
             InitializeComponent();
             axWindowsMediaPlayer1.settings.setMode("loop", true);
+            _zeroAdjustment = zeroAdjustment;
+            _uc1M1Zero = uc1M1Zero;
         }
 
         private void zeroB_Click(object sender, EventArgs e)
+        {
+            _zeroAdjustment.GetZeroAdjustmentValue();
+
+            if (_uc1M1Zero.IsMeasureDoneRight == true)
+            {
+                IsZeroAdjustmentMeasured = true;
+                this.Close();
+            }
+            if (_uc1M1Zero.IsMeasureDoneRight == false)
+            {
+                MessageBox.Show("Nulpunktsjustering ikke foretaget korrekt, se videoen igen");
+            }
+            else
+                MessageBox.Show("Nogete gik galt");
+
+
+        }
+
+        private void ZeroAdjustmentGUI_Load(object sender, EventArgs e)
         {
 
         }
